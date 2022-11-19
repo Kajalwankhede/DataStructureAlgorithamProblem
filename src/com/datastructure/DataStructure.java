b55bdcc7a0408ea52b5469928049b34b60783d46
Kajalwankhede <kajuwankhede1510@gmail.com>
2022-11-19 19:49:22 +0530

    Data strucure Problem

---
+++ src/com/datastructure/DataStructure.java
@@ -0,0 +1,7 @@
+package com.datastructure;
+
+public class DataStructure {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Data Structure Algorithm Program");
+    }
+}
